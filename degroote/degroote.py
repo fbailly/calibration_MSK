30f28083af895203cbabfde6c096c5cb2b4b557d
fbailly <francois.bailly@inria.fr>
2022-03-08 15:54:20 +0100

    Added muscle stretch analysis

--- degroote/degroote.py
+++ degroote/degroote.py
@@ -217,21 +217,43 @@ for ft0 in np.arange(0.2, 0.8, 0.05):
 plt.suptitle('Opensim-Thelen ODE for muscle tendon equilibrium')
 
 plt.figure()
+plt.subplot(121)
 mt_ratios = np.arange(0.1, 1.5, 0.1)
 lt_sls = np.arange(0.1, 0.4, 0.05)
 lms = np.zeros((mt_ratios.shape[0]))
 for j, lt_sl in enumerate(lt_sls):
     for i, mt_ratio in enumerate(mt_ratios):
-        lm_opt = lt_sl*mt_ratio  # optimal fiber len
-        lmt = lt_sl+lm_opt
+        lm_opt = lt_sl * mt_ratio  # optimal fiber len
+        lmt = lt_sl+np.cos(alpha0)*lm_opt
         lm0 = (lmt - lt_sl) / np.cos(alpha0)  # initial guess for the ivp problem (further noised)
         sol = solve_ivp(lm_ode_opensim, (0, tf), y0=np.array([lm0]), args=(a,), dense_output=True, method='RK23')
         lms[i] = sol.y[0, -1]
-    plt.plot(mt_ratios, lms, 'x', label=f'lt_sl={lt_sl:0.2f}', c=sns.color_palette()[j])
+        plt.plot(sol.y[0, :], c=sns.color_palette()[j % 10])
+    plt.plot(mt_ratios, lms, 'x', label=f'lt_sl={lt_sl:0.2f}', c=sns.color_palette()[j % 10])
     xx, yy = np.polyfit(mt_ratios, lms, 1)
-    plt.plot(mt_ratios, mt_ratios*xx+yy, c=sns.color_palette()[j])
+    # plt.plot(lmts, lmts*xx+yy, c=sns.color_palette()[i%10])
 plt.xlabel('muscle/tendon length ratios')
 plt.ylabel('Muscle length at equilibrium')
 plt.title('Muscle length at equilibrium for varying muscle/tendon length ratio\nOpensim-Thelen (2003)')
+plt.legend()
+plt.subplot(122)
+siz = 10
+lms = np.zeros((siz))
+mt_ratio = 1
+for j, lt_sl in enumerate(lt_sls):
+    lm_opt = lt_sl * mt_ratio  # optimal fiber len
+    lm_proj = np.cos(alpha0)*lm_opt
+    lmts = np.linspace(lt_sl+lm_proj*0.8, lt_sl+lm_proj*1.2, num=siz)
+    for i, lmt in enumerate(lmts):
+        lm0 = (lmt - lt_sl) / np.cos(alpha0)  # initial guess for the ivp problem (further noised)
+        sol = solve_ivp(lm_ode_opensim, (0, tf), y0=np.array([lm0]), args=(a,), dense_output=True, method='RK23')
+        lms[i] = sol.y[0, -1]
+        plt.plot(sol.y[0, :], c=sns.color_palette()[j % 10])
+    plt.plot(lmts/lt_sl, lms, 'x', label=f'lt_sl={lt_sl:0.2f}', c=sns.color_palette()[j % 10])
+    xx, yy = np.polyfit(lmts/lt_sl, lms, 1)
+    # plt.plot(lmts/lt_sl, lmts/lt_sl*xx+yy, c=sns.color_palette()[i%10])
+plt.xlabel('normalized muscle stretch (lmt/lt_sl)')
+plt.ylabel('Muscle length at equilibrium')
+plt.title('Muscle length at equilibrium for varying muscle stretch\n and mt_ratio = 1. Opensim-Thelen (2003)')
 plt.legend()
 plt.show()
